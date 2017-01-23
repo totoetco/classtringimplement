009be90e29559d0b6328f97414da98879cf71e50
ThomasB <thomas.breniere@insa-lyon.fr>
2017-01-23 16:24:37 +0100

    ajout du constructeur char*

--- String.cpp
+++ String.cpp
@@ -5,3 +5,13 @@
 void String::Afficher(void){
 ...
 }
+
+const char* String::c_str() const
+{
+    char rep[len+1];
+    for(size_t i=0 ; i<len ; i++) {
+        rep[i] = str[i];
+    }
+    rep[len] = '\0';
+    return rep;
+}
