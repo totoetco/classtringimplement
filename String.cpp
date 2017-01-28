4ad77f609561cf1aa2dae09c3d9ee89e0be7c793
Thomas B <thomas.breniere@insa-lyon.fr>
2017-01-28 10:15:39 +0100

    change orders, adding last missing functions

--- String.cpp
+++ String.cpp
@@ -2,15 +2,71 @@
 #include <cstdlib>
 #include "String.h"
 
-void String::Afficher(void){
-    
+
+void String::Afficher (void)
+{
     for(size_t i=0 ; i<len ; i++) {
         printf("%c",str[i]);
     }
     printf("\n");
 }
 
-void String::resize(size_t n){
+void String::reserve(size_t n) 
+{
+    if(n != cpty) {
+        if(n<len){
+            cpty = len;
+        }
+        else {
+            cpty = n;
+        }
+        char* t = new char[cpty];
+        for(size_t i=0 ; i<len ; i++) {
+            t[i] = str[i];
+        }
+        delete [] str;
+        str = t;
+    }
+}
+
+size_t String::size() const
+{
+    return len;
+}
+
+size_t String::length() const
+{
+    return len;
+}
+
+size_t String::capacity() const
+{
+    return cpty;
+}
+
+bool String::empty() const
+{
+    return !(len);
+}
+
+void String::clear()
+{
+    len = 0;
+}
+
+const char* String::c_str() const
+{
+    char rep[len+1];
+    for(size_t i=0 ; i<len ; i++) {
+        rep[i] = str[i];
+    }
+    rep[len] = '\0';
+    return rep;
+}
+
+size_t String::max_size() const
+{
+    return -1;
 }
 
 void String::resize(size_t n, char c) {
@@ -31,77 +87,7 @@ void String::resize(size_t n, char c) {
     }
 }
 
-//teste si la chaine est vide
-
-bool String::empty() const
-{
-    return !(len);
-}
-
-// réserve de la mémoire en plus pour stocker notre variable,
-// selon la capacité passe en parametre
-
-void String::reserve(size_t n) 
-{
-    if(n != cpty) {
-        if(n<len){
-            cpty = len;
-        }
-        else {
-            cpty = n;
-        }
-        char* t = new char[cpty];
-        for(size_t i=0 ; i<len ; i++) {
-            t[i] = str[i];
-        }
-        str = t;
-		delete [] str;
-    }
-}
-
-
-
-
-void String::clear()
-{
-  len = 0;
-}
-//----------------------------getters
-size_t String::size() const
-{
-    return len;
-}
-
-    
-size_t String::length() const
-{
-    return len;
-}
-
-size_t String::capacity() const
-{
-    return cpty;
-}
-
-const char* String::c_str() const
-{
-    char rep[len+1];
-    for(size_t i=0 ; i<len ; i++) {
-        rep[i] = str[i];
-    }
-    rep[len] = '\0';
-    return rep;
-}
-
-
-//--------Les operateurs.
-
-// le destructeur
-
-String::~String ( )
-{
-delete [] str;
-} 
+// Surcharge d'opérateurs
 
 String String::operator+ (char unchar)
 {
@@ -112,49 +98,6 @@ String String::operator+ (char unchar)
     s.str[s.length()] = unchar;
     s.len++;
     return s;
-}
-
-String String::operator+ (const String st) {
-	String s(*this);
-    size_t si = st.length();
-    if(s.length()+si > s.capacity()) {
-        s.reserve(s.length()+si);
-    }
-    
-    for(size_t i=0 ; i<si ; i++) {
-        s.str[s.length()+i] = st.str[i];
-    }
-    s.len += si;
-    return s;	
-}
-String & String::operator= (const String & unStr)
-{
-    len = unStr.length();
-    cpty = unStr.capacity();
-
-    delete [] str;
-    str = new char[cpty];
-    for(size_t i=0 ; i<len ; i++) {
-        str[i] = unStr.str[i];
-    }
-    return *this;
-}
-
-String & String::operator= (const char * uneChaine)
-{
-    size_t size = 0;
-    while(uneChaine[size++] != '\0') {}
-    size--;
-    
-    cpty = len = size;
-    delete [] str;
-    str = new char[cpty];
-    
-    for(size_t i=0 ; i<len ; i++) {
-        str[i] = uneChaine[i];
-    }
-    
-    return *this;
 }
 
 String String::operator+ (const char* uneChaine)
@@ -175,4 +118,109 @@ String String::operator+ (const char* uneChaine)
     return s;
 }
 
+String String::operator+ (const String st)
+{
+    String s(*this);
+    size_t si = st.length();
+    if(s.length()+si > s.capacity()) {
+        s.reserve(s.length()+si);
+    }
+    
+    for(size_t i=0 ; i<si ; i++) {
+        s.str[s.length()+i] = st.str[i];
+    }
+    s.len += si;
+    return s;
+}
+
+String & String::operator= (const String & unStr)
+{
+    len = unStr.length();
+    cpty = unStr.capacity();
+
+    delete [] str;
+    str = new char[cpty];
+    for(size_t i=0 ; i<len ; i++) {
+        str[i] = unStr.str[i];
+    }
+    return *this;
+}
+
+String & String::operator= (char unchar)
+{
+    delete [] str;
+    str = new char[1];
+    str[0] = unchar;
+    cpty = 1;
+    len = 1;
+    
+    return *this;
+}
+
+String & String::operator= (const char * uneChaine)
+{
+    size_t size = 0;
+    while(uneChaine[size++] != '\0') {}
+    size--;
+    
+    cpty = len = size;
+    delete [] str;
+    str = new char[cpty];
+    
+    for(size_t i=0 ; i<len ; i++) {
+        str[i] = uneChaine[i];
+    }
+    
+    return *this;
+}
+
+//Constructeurs - destructeur
+String::String ( const String & unStr )
+
+{
+
+
+len = unStr.length();
+cpty = unStr.capacity();
+
+str = new char[cpty];
+for(size_t i=0 ; i<len ; i++) {
+    str[i] = unStr.str[i];
+}
+
+}
+
+
+String::String (size_t cap)
+
+{
+
+len = 0;
+cpty = cap;
+str = new char[cap];
+
+}
+
+String::String (const char* cstr)
+{
+
+    size_t size = 0;
+    while(cstr[size++] != '\0') {}
+    size--;
+    
+    cpty = len = size;
+    str = new char[size];
+    
+    for(size_t i=0 ; i<size ; i++) {
+        str[i] = cstr[i];
+    }
+}
+//Fin du Constructeur2
+
+String::~String ( )
+
+{
+
+delete [] str;
+}
 
